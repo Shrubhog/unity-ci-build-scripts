59f1ef52ac3b540dfd27e2c7b56fff42a9afb911
Noah Garcia <37482938+Shrubhog@users.noreply.github.com>
2019-10-08 17:21:40 -0500

    temporary

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -5,5 +5,5 @@ set -x
 mkdir -p /root/.cache/unity3d
 mkdir -p /root/.local/share/unity3d/Unity/
 set +x
-echo "Writing $1 to Unity license."
-cp $1 /root/.local/share/unity3d/Unity/Unity_lic.ulf
+#echo "Writing $1 to Unity license."
+#cp $1 /root/.local/share/unity3d/Unity/Unity_lic.ulf
