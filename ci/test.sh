76c48726537706104c40968b52c40517b4e4f861
Noah Garcia <37482938+Shrubhog@users.noreply.github.com>
2019-10-08 17:27:49 -0500

    typo

--- ci/test.sh
+++ ci/test.sh
@@ -10,8 +10,8 @@ ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x2
   -testPlatform $TEST_PLATFORM \
   -testResults $(pwd)/$TEST_PLATFORM-results.xml \
   -logFile \
+  -batchmode \
   -manualLicenseFile ../Unity_v2018.x.ulf
-  -batchmode
 
 UNITY_EXIT_CODE=$?
 
